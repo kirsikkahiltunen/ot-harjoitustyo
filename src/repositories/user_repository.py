8b37e4aa94eab9ee1a968cf4d1e2a0fae7b08e9c
Kirsikka Hiltunen <kirsikka.hiltunen@gmail.com>
2024-04-28 12:46:17 +0300

    Debugging prints removed

--- src/repositories/user_repository.py
+++ src/repositories/user_repository.py
@@ -2,11 +2,9 @@ from database_connection import get_database_connection
 
 class UserRepository:
     def __init__(self):
-        print("initalisoi")
         self._connection = get_database_connection()
 
     def find_user_by_username(self, username):
-        print("hei")
         cursor = self._connection.cursor()
         cursor.execute("SELECT username FROM users WHERE username=?", (username,))
 
@@ -14,7 +12,6 @@ class UserRepository:
         return row
     
     def list_all(self):
-        print("ei toimi?")
         cursor = self._connection.cursor()
         cursor.execute("SELECT * FROM users")
         rows=cursor.fetchall()
@@ -22,7 +19,6 @@ class UserRepository:
 
 
     def create_user(self, user, password):
-        print("asdf")
         already_existing = self.find_user_by_username(user)
         if already_existing:
             return False
