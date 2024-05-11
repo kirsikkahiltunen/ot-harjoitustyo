8576b59d7c89d6cffa083d0c98c5947dde24bdd6
Kirsikka Hiltunen <kirsikka.hiltunen@gmail.com>
2024-05-11 12:54:35 +0300

    Added docstrings. Added notification message if username or password is incorrect.

--- src/ui/login_view.py
+++ src/ui/login_view.py
@@ -3,7 +3,16 @@ from repositories.user_repository import UserRepository
 
 
 class LoginView:
+    """Luokka, joka vastaa sisäänkirjautumisnäkymästä.
+    """
     def __init__(self, root, _show_create_user, _show_exercise_list_view):
+        """Luokan konstruktori, jossa kutsutaan UserRepository:a sekä luodaan kirjautumisnäkymä.
+
+        Args:
+            root: Tkinter:in juuri
+            _show_create_user: arvo, jota kutsutaan, kun siirrytään käyttäjätunnuksen luonti näkymään.
+            _show_exercise_list_view: arvo, jota kutsutaan, kun siirrytään tehtävälista näkymään.
+        """
         self._root = root
         self._username_entry = None
         self._password_entry = None
@@ -15,12 +24,21 @@ class LoginView:
         self.login_frame()
 
     def pack(self):
+        """Näyttää login_frame()-funktion määrittämän näkymän.
+        """
         self._frame.pack(fill=constants.X)
 
     def destroy(self):
+        """Tuhoaa viimeisimmän näkymän.
+        """
         self._frame.destroy()
 
-    def login_frame(self):
+    def login_frame(self, message=None):
+        """Määrittää kirjautumissivun komponentit ja asettelun.
+
+        Args:
+            message: Käyttäjälle näytettävä viesti. Defaults to None.
+        """
 
         self._frame = ttk.Frame(master=self._root)
 
@@ -55,7 +73,7 @@ class LoginView:
         self._password_entry.grid(row=2, column=1, sticky=(
             constants.E, constants.W), padx=5, pady=5)
 
-        button.grid(row=3, column=0, columnspan=2, sticky=(
+        button.grid(row=3, column=1, columnspan=1, sticky=(
             constants.E, constants.W), padx=5, pady=5)
 
         create_user_label.grid(row=4, column=0, padx=10, pady=10)
@@ -65,15 +83,31 @@ class LoginView:
 
         self._root.grid_columnconfigure(1, weight=1, minsize=300)
 
+        if message:
+            message_label = ttk.Label(
+                master=self._frame, text=message)
+            message_label.grid(row=5, column=0, padx=10, pady=10)
+
     def _login_handler(self):
+        """Käsittelee sisäänkirjautumisen.
+        """
         self._username = self._username_entry.get()
         self._password = self._password_entry.get()
 
         if self.user_repo.login(self._username, self._password):
             self.viewnext()
+        else:
+            self.destroy()
+            message = "Käyttäjätunnus tai salasana väärin"
+            self.login_frame(message)
+            self.pack()
 
     def viewnext(self):
+        """Vie käyttäjän tehtävälistaus näkymään.
+        """
         self._show_exercise_list_view()
 
     def _create_new_handler(self):
+        """Vie käyttäjän uuden tunnuksen luonti näkymään.
+        """
         self._show_create_user_view()
